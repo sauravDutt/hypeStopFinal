5147a928c9216eec5ca5c9ef56dc6db39d1ed9ef
sauravDutt <sauravd069@gmail.com>
2022-06-05 20:33:54 +0530

    Just added some more stuff

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
-const Home = () => {
-  return (
-    <>
-        HeroBanner
+import {client} from '../lib/client';
 
+import {Product, FooterBanner, HeroBanner} from '../components'
+
+const Home = ({products, bannerData}) => (
+   <div>
+        <HeroBanner heroBanner={bannerData.length && bannerData[0]}/>
         <div className="products-heading">
           <h2>
             Best Selling Products
@@ -13,12 +15,24 @@ const Home = () => {
         </div>
 
         <div className="products-container">
-          {['Product 1', 'Product 2'].map((product) => product)}
+          {products?.map((product) => product.name)}
         </div>
 
-        Footer
-    </>
-  )
+        <FooterBanner/>
+  </div>
+
+);
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props :{ products, bannerData}
+  }
 }
 
 export default Home;
