cd877476edad04a9ee8a7d88b6bf967c2ae93c23
sauravDutt <sauravd069@gmail.com>
2022-06-06 11:16:35 +0530

    Just added some more stuff

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { AiFillInstagram } from 'react-icons/ai';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer-container'>
+      <p>2022 HYPESTOP, All rights reserved</p>
+      <p className='icons'>
+        <AiFillInstagram />
+      </p>
+    </div>
   )
 }
 
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,17 +1,31 @@
 import React from 'react'
-
+import {motion} from 'framer-motion';
 import Link from 'next/link'
 import Image from 'next/image';
-import {AiOutlineShopping} from 'react-icons';
+import {AiOutlineShopping} from 'react-icons/ai';
 
 const Navbar = () => {
   return (
     <div className="navbar-container">
       <p className='logo'>
         <Link href="/">
-          <Image src="/logo.jpeg" className='logoImg' alt="logo" width="95" height="50"/>
+          <Image 
+            src="/logo.jpeg" 
+            className='logoImg' 
+            alt="logo" 
+            width="95" 
+            height="50" 
+            />
         </Link>
       </p>
+      <button
+        type='button'
+        className='cart-icon'
+        onAbort=""
+      >
+        <AiOutlineShopping />
+        <span className='cart-item-qty'>1</span>
+      </button>
     </div>
   )
 }
