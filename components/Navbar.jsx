a6476453e750103b1f140c22489762048835d9a6
sauravDutt <sauravd069@gmail.com>
2022-06-07 01:39:58 +0530

    Just added some more stuff

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,7 +4,12 @@ import Link from 'next/link'
 import Image from 'next/image';
 import {AiOutlineShopping} from 'react-icons/ai';
 
+import {Cart} from './';
+import { useStateContext } from '../context/StateContext';
+
 const Navbar = () => {
+  const {showCart, setShowCart, totalQuantities} = useStateContext();
+
   return (
     <div className="navbar-container">
       <p className='logo'>
@@ -21,11 +26,13 @@ const Navbar = () => {
       <button
         type='button'
         className='cart-icon'
-        onAbort=""
+        onClick={() => setShowCart(true)}
       >
         <AiOutlineShopping />
-        <span className='cart-item-qty'>1</span>
+        <span className='cart-item-qty'>{totalQuantities}</span>
       </button>
+
+      {showCart && <Cart /> }
     </div>
   )
 }
--- context/StateContext.js
+++ context/StateContext.js
@@ -6,10 +6,54 @@ const Context = createContext();
 
 export const StateContext = ({ children }) => {
     const [showCart, setShowCart] = useState(false);
-    const [cartItems, setCartItems] = useState();
-    const [totalPrice, setTotalPrice] = useState();
-    const [totalQuantities, setTotalQuantities] = useState();
+    const [cartItems, setCartItems] = useState([]);
+    const [totalPrice, setTotalPrice] = useState(0);
+    const [totalQuantities, setTotalQuantities] = useState(0);
     const [qty, setQty] = useState(1);
+
+    let foundProduct;
+    let index;
+
+    const onAdd = (product, quantity) => {
+        const checkProductInCart = cartItems.find((item) => item._id === product._id);
+
+        setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity);
+        setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity);
+
+        if(checkProductInCart) {
+            
+            const updatedCartItems = cartItems.map((cartProduct) => {
+                if(cartProduct._id === product._id) return {
+                    ...cartProduct,
+                    quantity: cartProduct.quantity + quantity
+                }
+            });
+            setCartItems(updatedCartItems);
+        } else {
+            product.quantity = quantity;
+            setCartItems([...cartItems, { ...product }])
+        }
+
+        toast.success(`${qty} ${product.name} added to the cart.`)
+    }
+
+    const toggleCartItemQuantity = (id, value) => {
+        foundProduct = cartItems.find((item) => item._id === id);
+        index = cartItems.findIndex((product) => product._id === id);
+        const newCartItems = cartItems.filter((item) => item._id !== id);
+
+        if(value === 'inc') {
+            setCartItems([...newCartItems, { ...foundProduct, quantity: foundProduct.quantity + 1 }])
+            setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price);
+            setTotalQuantities(prevTotalQuantities => prevTotalQuantities + 1);
+        }else if(value === 'dec') {
+            if(foundProduct.quantity > 1) {
+                setCartItems([...newCartItems, { ...foundProduct, quantity: foundProduct.quantity - 1 }])
+                setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price);
+                setTotalQuantities(prevTotalQuantities => prevTotalQuantities - 1);
+            }
+        }
+    }
 
     const incQty = () => {
         setQty((prevQty) => prevQty +1);
@@ -27,12 +71,15 @@ export const StateContext = ({ children }) => {
         <Context.Provider
             value = {{
                 showCart,
+                setShowCart,
                 cartItems,
                 totalPrice,
                 totalQuantities,
                 qty,
+                toggleCartItemQuantity,
                 incQty,
-                decQty
+                decQty,
+                onAdd
             }}
         >
             {children}
