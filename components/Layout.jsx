5d4b52c9bd81145db82ad98e80c97526df639bf7
sauravDutt <sauravd069@gmail.com>
2022-06-10 15:40:39 +0530

    Just added some more stuff

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -10,6 +10,14 @@ const Layout = ({children}) => {
         <title>
           HYPESTOP
         </title>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-CE0ECH2BLR"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments)}
+          gtag('js', new Date());
+
+          gtag('config', 'G-CE0ECH2BLR');
+        </script> 
       </Head>
       <header>
         <Navbar />
