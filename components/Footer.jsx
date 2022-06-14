e845a9d4cb2f8d8d907ed77c0d553698c3b5b57f
sauravDutt <sauravd069@gmail.com>
2022-06-14 16:39:21 +0530

    Just added some more stuff

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import {AiFillInstagram} from 'react-icons/ai';
 
 const Footer = () => {
   return (
     <div className='footer-container'>
-      <p>2022 HYPESTOP, All rights reserved</p>
+      <p>2022 HYPESTOP, All rights reserved  </p>
+      <p> Follow us on Instagram at <br /><a href='https://www.instagram.com/hypestopindia/' target='_blank' rel='noreferrer' className="instagram">
+      <AiFillInstagram />/hypestopindia
+      </a></p>
     </div>
   )
 }
