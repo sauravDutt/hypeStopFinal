b0c93ed17c00786d83d0ff1d6dd73bc853bfa3c9
sauravDutt <sauravd069@gmail.com>
2022-06-23 12:07:36 +0530

    Just added some more stuff

--- sanity_hypestop/schemas/schema.js
+++ sanity_hypestop/schemas/schema.js
@@ -3,9 +3,10 @@ import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 import product from './product';
 import banner from './banner';
+import preorders from './preorders';
 
 export default createSchema({
  
   name: 'default',
-  types: schemaTypes.concat([product, banner]),
+  types: schemaTypes.concat([preorders, product, banner]),
 })
