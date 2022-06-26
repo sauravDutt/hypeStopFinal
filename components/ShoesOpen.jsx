86896cefc4983c628d047e43d08e11b1a5c4c6df
sauravDutt <sauravd069@gmail.com>
2022-06-26 15:09:18 +0530

    Just added some more suff

--- components/ShoesOpen.jsx
+++ components/ShoesOpen.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Link from 'next/link';
 import {useStateContext} from '../context/StateContext';
 import {AiOutlineClose} from 'react-icons/ai'
 import {GiRunningShoe} from 'react-icons/gi';
 
-const ShoesOpen = () => {
+import { urlFor } from '../lib/client';
+
+const ShoesOpen = ({ product: { image, name, slug, price } }) => {
 
     const {setShowShoes} = useStateContext();
 
@@ -15,6 +18,18 @@ const ShoesOpen = () => {
                 <AiOutlineClose />
             </button>
             <h2 className='navBtn-title'><GiRunningShoe/> | Shoes</h2>
+            <Link href={`/product/${slug.current}`}>
+                <div className="product-card">
+                <img 
+                    src={urlFor(image && image[0])}
+                    width={250}
+                    height={250}
+                    className="product-image"
+                />
+                <p className="product-name">{name}</p>
+                <p className="product-price">₹{price}</p>
+                </div>
+            </Link>
         </div>
     );
 }
