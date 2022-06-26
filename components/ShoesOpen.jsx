c972f2c43ec327e0ead0046063d661590654cca9
sauravDutt <sauravd069@gmail.com>
2022-06-26 15:13:20 +0530

    Just added some more stuff

--- components/ShoesOpen.jsx
+++ components/ShoesOpen.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import Link from 'next/link';
 import {useStateContext} from '../context/StateContext';
 import {AiOutlineClose} from 'react-icons/ai'
 import {GiRunningShoe} from 'react-icons/gi';
 
-import { urlFor } from '../lib/client';
-
-const ShoesOpen = ({ product: { image, name, slug, price } }) => {
+const ShoesOpen = () => {
 
     const {setShowShoes} = useStateContext();
 
@@ -18,18 +15,6 @@ const ShoesOpen = ({ product: { image, name, slug, price } }) => {
                 <AiOutlineClose />
             </button>
             <h2 className='navBtn-title'><GiRunningShoe/> | Shoes</h2>
-            <Link href={`/product/${slug.current}`}>
-                <div className="product-card">
-                <img 
-                    src={urlFor(image && image[0])}
-                    width={250}
-                    height={250}
-                    className="product-image"
-                />
-                <p className="product-name">{name}</p>
-                <p className="product-price">₹{price}</p>
-                </div>
-            </Link>
         </div>
     );
 }
