d69a4852ef90b93537ceb960269cd40be679ab8d
sauravDutt <sauravd069@gmail.com>
2022-07-07 14:16:14 +0530

    Just added some more stuff

--- components/ShoesOpen.jsx
+++ components/ShoesOpen.jsx
@@ -2,10 +2,8 @@ import React from 'react';
 import {useStateContext} from '../context/StateContext';
 import {AiOutlineClose} from 'react-icons/ai'
 import {GiRunningShoe} from 'react-icons/gi';
-import Product from './Product';
-import {client} from '../lib/client';
 
-const ShoesOpen = ({products}) => {
+const ShoesOpen = () => {
 
     const {setShowShoes} = useStateContext();
 
@@ -18,22 +16,10 @@ const ShoesOpen = ({products}) => {
             </button>
             <h2 className='navBtn-title'><GiRunningShoe/> | Shoes</h2>
             
-            <div className="products-container">
-                {products?.map((product) => <Product key={product.id} product={product} />)}
-            </div>
             
         </div>
     );
 }
-
-export const getServerSideProps = async () => {
-    const query = '*[_type == "product"]';
-    const products = await client.fetch(query);
-  
-    return {
-      props :{ products}
-    }
-  }
 
 
 export default ShoesOpen;
